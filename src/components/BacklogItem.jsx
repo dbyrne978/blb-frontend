eb40c44d6ff7a07ef59e9dc0a236d6a6db3b0cab
Daniel Byrne <dbyrne978@gmail.com>
2024-01-11 12:28:54 -0700

    Change format to appear as icon

--- src/components/BacklogItem.jsx
+++ src/components/BacklogItem.jsx
@@ -4,6 +4,10 @@ import CardHeader from '@mui/material/CardHeader';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import TvIcon from '@mui/icons-material/Tv';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
 import PropTypes from 'prop-types'
 
 const BacklogItem = ({ backlogItem }) => {
@@ -12,7 +16,10 @@ const BacklogItem = ({ backlogItem }) => {
       <CardHeader
         avatar={
           <Avatar aria-label="recipe">
-            {backlogItem.format}
+            {backlogItem.format == 'Game' && <SportsEsportsIcon/>}
+            {backlogItem.format == 'Book' && <AutoStoriesIcon/>}
+            {backlogItem.format == 'TV Show' && <TvIcon/>}
+            {backlogItem.format == 'Movie' && <LocalMoviesIcon/>}
           </Avatar>
         }
         action={
